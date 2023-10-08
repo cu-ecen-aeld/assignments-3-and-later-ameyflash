2988225c43b7af1d014e5603dbaab3f272f8bbef
ameyflash <amey.hema@gmail.com>
2023-10-08 00:46:08 -0600

    Updated start stop script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -12,9 +12,9 @@ fi
 case "$1" in
 	start)
 		echo "Start AESD socket application"
-		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
 		# local --> -x /home/amey/AESD/assignment-5-part-1-ameyflash/server/aesdsocket -- -d
-		# buildroot --> -a /usr/bin/aesdsocket
+		# buildroot --> -a /usr/bin/aesdsocket -- -d
 		;;
 	stop)
 		echo "Stop AESD socket application"
